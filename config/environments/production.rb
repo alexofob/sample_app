5ed70d355fe8ede2ea3203c1465e280a2649d1c8
Alex Ofori <alexofob@yahoo.com>
2013-04-29 12:49:39 +0000

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,8 +60,11 @@ SampleApp::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Force all access to the app over SSL, use Strict-Transport-Security, 
+  # and use secure cookies.
+  config.force_ssl = true
 end
